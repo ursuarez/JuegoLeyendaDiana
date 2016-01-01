f53a0af9bc3b8ea1b8eb0e4edc1ce5d0eb6a36be
ursuarez <ur.suarez@gmail.com>
2015-12-31 19:15:38 -0600

    Agregado imagen diana-frente.png

--- tifis.js
+++ tifis.js
@@ -1,18 +1,48 @@
 var tablero;
-var fondo = {
-	imagenURL: "img/fondo.png"
 
+var fondo = {
+	imagenURL: "img/fondo.png",
+	imagenOK: false
 };
 
-function inicio (){
+var tifis = {
+	x: 150,
+	y: 200,
+	frenteURL: "img/diana-frente.png",
+	frenteOK: false
+};
+
+function inicio(){
 	//alert("Arranca correctamente");
 	var canvas = document.getElementById("campo");
 	tablero = canvas.getContext("2d");
+	
 	fondo.imagen = new Image();
 	fondo.imagen.src = fondo.imagenURL;
-	fondo.imagen.onload = dibujarFondo();
+	fondo.imagen.onload = confirmarFondo();
+
+	tifis.frente = new Image();
+	tifis.frente.src = tifis.frenteURL;
+	tifis.frente.onload = confirmarFrente();
 }
 
-function dibujarFondo(){
-	tablero.drawImage(fondo.imagen, 0, 0);
+function confirmarFondo(){
+	fondo.imagenOK = true;
+	dibujar();
+}
+
+function confirmarFrente(){
+	tifis.frenteOK = true;
+	dibujar();
+}
+
+
+function dibujar(){
+	if (fondo.imagenOK == true) {
+		tablero.drawImage(fondo.imagen, 0, 0);	
+	}
+	if (tifis.frenteOK == true) {
+		tablero.drawImage(tifis.frente, tifis.x, tifis.y);	
+	}
+
 }
